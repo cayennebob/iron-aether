ec9abbdcfd2e578f7fc8454b540bfa17b697428f
Cayenne Geis <cayennes@gmail.com>
2015-04-26 13:25:06 -0400

    Randomness in attack effect

--- src/Main.elm
+++ src/Main.elm
@@ -1,70 +1,88 @@
-import Graphics.Element as Element
+import Graphics.Element as Element exposing (Element)
 import Graphics.Input as Input
+import Random
+import Time exposing (Time)
+
+import Dice
 
 -- MODEL
 
 type alias Character = { totalHP : Int
                        , currentHP : Int
                        , attack : Int
+                       , defense : Int
                        }
 
 type alias Game = { player : Character
                   , enemy : Character
+                  , seed : Random.Seed
+                  , seedInitialized : Bool
                   }
 
-initCharacter : Int -> Int -> Character
-initCharacter hp attack =
+initCharacter : Int -> Int -> Int -> Character
+initCharacter hp attack defense =
     { totalHP = hp
     , currentHP = hp
     , attack = attack
+    , defense = defense
     }
 
 initGame : Game
-initGame = { player = initCharacter 100 10
-           , enemy = initCharacter 100 9
+initGame = { player = initCharacter 100 10 10
+           , enemy = initCharacter 50 10 10
+           , seed = Random.initialSeed 0 -- make types happy; not used
+           , seedInitialized = False
            }
 
 -- UPDATE
 
 type Attack = BasicAttack
 
-update : Attack -> Game -> Game
-update attack model =
-    model |> doPlayerAttack attack
+update : (Time, Attack) -> Game -> Game
+update (time, attack) model =
+    model |> Dice.ensureSeed time
+          |> doPlayerAttack attack
           |> doEnemyAttack
+
+doAttack : Random.Seed -> Attack -> Character -> Character -> (Character, Random.Seed)
+doAttack seed attack attacker defender =
+    let (roll, newSeed) = Dice.basicRoll seed
+        damage = max 0 (roll + attacker.attack - defender.defense + 5)
+        updatedHP = defender.currentHP - damage
+    in ({ defender | currentHP <- updatedHP }, newSeed)
 
 doPlayerAttack : Attack -> Game -> Game
 doPlayerAttack attack model =
-    let enemy = model.enemy
-        player = model.player
-        updatedHP = (enemy.currentHP - player.attack)
-        updatedEnemy = { enemy | currentHP <- updatedHP }
-    in { model | enemy <- updatedEnemy }
+    let (updatedEnemy, newSeed) =
+            doAttack model.seed attack model.player model.enemy
+    in { model | enemy <- updatedEnemy
+               , seed <- newSeed }
 
 doEnemyAttack : Game -> Game
 doEnemyAttack model =
-    let player = model.player
-        enemy = model.enemy
-        updatedHP = player.currentHP - enemy.attack
-        updatedPlayer = { player | currentHP <- updatedHP }
-    in { model | player <- updatedPlayer }
+    let (updatedPlayer, newSeed) =
+            doAttack model.seed BasicAttack model.enemy model.player
+    in { model | player <- updatedPlayer
+               , seed <- newSeed }
 
 -- SIGNALS
 
 game : Signal.Signal Game
 game =
-    Signal.foldp update initGame attack.signal
+    attack.signal
+        |> Time.timestamp
+        |> Signal.foldp update initGame
 
 -- VIEW
 
 attack : Signal.Mailbox Attack
 attack = Signal.mailbox BasicAttack
 
-main : Signal Element.Element
+main : Signal Element
 main =
     Signal.map view game
 
-view : Game -> Element.Element
+view : Game -> Element
 view game =
     Element.flow Element.down
         [ Element.show game
